d2c366306406824827dc281acea86742a5125109
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-01-05 17:22:14 +0900

    삼각함수 연습

---
+++ BulletCircleTest.py
@@ -0,0 +1,60 @@
+import pygame
+import math
+
+pygame.init()
+
+ourScreen = pygame.display.set_mode((500,500), pygame.DOUBLEBUF)
+
+pygame.display.set_caption('원형탄 테스트')
+
+TARGET_FPS = 30
+
+clock = pygame.time.Clock()
+finish = False
+center = (250,250)
+radius = 200
+count = 1
+pivot = (math.pi * 2) * 0.75
+
+score = 82349
+num = score
+numbers = []
+
+while num > 0:
+    numbers.insert(0,num % 10)
+    print(numbers[0])
+    num = int(num / 10)
+
+print(numbers)
+
+
+while not finish:
+
+    ourScreen.fill((0,0,0))
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            finish = True
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            count = count + 1
+
+
+        x = 0
+        y = 0
+        pygame.draw.circle(ourScreen,(255,255,255,0.5),center,5)
+
+
+        for i in range(count):
+            x = int(center[0] + math.cos(((math.pi * 2) * (i / count)) + pivot) * radius)
+            y = int(center[1] + -math.sin(((math.pi * 2) * (i / count))+ pivot) * radius)
+            pygame.draw.circle(ourScreen,(255,255,255,127),(x,y),5)
+
+
+        pygame.display.flip()
+
+
+    clock.tick(TARGET_FPS)
+
+
